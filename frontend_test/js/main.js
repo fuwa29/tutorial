171a2d94e5e3f3f3927d91911010d7485fc07a85
Wataru Fuchu <fuchuwat@gmail.com>
2019-04-01 23:00:57 +0900

    Finished Part2-1 Tetrimino

--- frontend_test/js/main.js
+++ frontend_test/js/main.js
@@ -1,1 +1,35 @@
-document.getElementById("hello_text").textContent = "上書きしたテキストです 初めてのJavascript";
+/* ---------------------------------------------------------------------------*/
+/* First Javascript app TETRIS */
+/* width =10boxes        */
+/* height=20boxes        */
+/* ---------------------------------------------------------------------------*/
+/* idで指定されてるテキストを書き換えるテスト */
+document.getElementById("hello_text").textContent = "初めてのJavascript";
+
+/*
+var count = 0;
+setInterval(function() {
+	// 何回目かをカウント変数を増やしていく
+	count++;
+	document.getElementById("hello_text").textContent = "初めてのJavascript(" + count + ")";
+  },1000);
+*/
+
+var td_array = document.getElementsByTagName("td");
+var index = 0;
+for (var row = 0; row < 20; row++) {
+	for (var col = 0; col < 10; col++) {
+//		td_array[index].textContent = index;
+//		td_array[index].textContent = "[" + row + "," + col + "]";
+		index++;
+	}
+}
+
+for (var row = 0; row < 20; row++) {
+	for (var col = 0; col < 10; col++) {
+//		td_array[index].textContent = index;
+		td_array[index].textContent = "[" + row + "," + col + "]";
+		index++;
+	}
+}
+
